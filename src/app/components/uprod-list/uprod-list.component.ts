b7ef79d6e682fd0a0625f1a006d939405f782609
Jihen-Issaoui <77747967+Jihen-Issaoui@users.noreply.github.com>
2022-12-22 11:01:32 +0100

    Update uprod-list.component.ts

--- src/app/components/uprod-list/uprod-list.component.ts
+++ src/app/components/uprod-list/uprod-list.component.ts
@@ -29,85 +29,11 @@ const httpOptions = {
 
 export class UprodListComponent implements OnInit  {
 
-  thumbnail: any;
- // const rslt : Observable<any> = this.uprodService.SearchAll(this.uprodService.AuthHeader("https://api.uprodit.com/v1/search/all?startIndex=0&maxResults=10&usecase=perso"));
-         
+  thumbnail: any;      
   tutorials?: Uprod;
   comwork?: Comwork[] = [];
   cowork? : Comwork ;
-  
-  filterTerm!: string;
-  userRecords = [
-    {
-      id: 1,
-      name: 'Leanne Graham',
-      username: 'Bret',
-      email: 'Sincere@april.biz',
-    },
-    {
-      id: 2,
-      name: 'Ervin Howell',
-      username: 'Antonette',
-      email: 'Shanna@melissa.tv',
-    },
-    {
-      id: 3,
-      name: 'Clementine Bauch',
-      username: 'Samantha',
-      email: 'Nathan@yesenia.net',
-    },
-    {
-      id: 4,
-      name: 'Patricia Lebsack',
-      username: 'Karianne',
-      email: 'Julianne.OConner@kory.org',
-    },
-    {
-      id: 5,
-      name: 'Chelsey Dietrich',
-      username: 'Kamren',
-      email: 'Lucio_Hettinger@annie.ca',
-    },
-    {
-      id: 6,
-      name: 'Mrs. Dennis Schulist',
-      username: 'Leopoldo_Corkery',
-      email: 'Karley_Dach@jasper.info',
-    },
-    {
-      id: 7,
-      name: 'Kurtis Weissnat',
-      username: 'Elwyn.Skiles',
-      email: 'Telly.Hoeger@billy.biz',
-    },
-    {
-      id: 8,
-      name: 'Nicholas Runolfsdottir V',
-      username: 'Maxime_Nienow',
-      email: 'Sherwood@rosamond.me',
-    },
-    {
-      id: 9,
-      name: 'Glenna Reichert',
-      username: 'Delphine',
-      email: 'Chaim_McDermott@dana.io',
-    },
-    {
-      id: 10,
-      name: 'Clementina DuBuque',
-      username: 'Moriah.Stanton',
-      email: 'Rey.Padberg@karina.biz',
-    },
-  ];
 
-
-//  image_id? : [];
-  
-/*
-    myControl = new FormControl('');
-    options: string[] = ['One', 'Two', 'Three'];
-    filteredOptions?: Observable<string[]>;
-*/
 
   constructor(private uprodService: UprodService,
     private route: ActivatedRoute,
@@ -119,26 +45,9 @@ export class UprodListComponent implements OnInit  {
     console.log(fileId) 
     this.AuthHeaderUprod();
 
-
-  
-    //const image_id = localStorage.getItem("names");
-    //console.log(image_id);
-
-    //this.AuthHeaderImg(image_id)
-
- /*   this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );*/
   }
-
-
- /* private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }*/
   
+  // Token AuthImage
 
   AuthHeaderImg(image_id : number, id : number): void {
   
@@ -179,6 +88,7 @@ SearchAllImg(image_id: any, id :any) : void {
 }
 
 
+  // Token AuthSearchAll
   AuthHeaderUprod(): void {
     this.uprodService.AuthHeader("https://api.uprodit.com/v1/search/all?startIndex=0&maxResults=10&usecase=perso")
       .subscribe(
